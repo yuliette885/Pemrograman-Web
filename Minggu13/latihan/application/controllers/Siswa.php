e2151842f5c9a5fcf9b422f40e0066bc696a0ffd
yuliette885 <yulietterindani07@gmail.com>
2020-06-14 15:43:38 +0700

    menambahkan folder minggu 13

---
+++ Minggu13/latihan/application/controllers/Siswa.php
@@ -0,0 +1,67 @@
+<?php
+
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+
+class Siswa extends REST_Controller
+{
+	function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    public function index_get()
+    {
+        $id = $this->get('id');
+        if ($id == '') {
+            $Siswa = $this->db->get('sekolah')->result();
+        } else {
+            $this->db->where('id', $id);
+            $Siswa = $this->db->get('sekolah')->result();
+        }
+        $this->response($Siswa, 200);
+    }
+
+        function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nama'          => $this->post('nama'),
+                    'alamat'    => $this->post('alamat'),
+					'logo' => $this->post('logo'));
+        $insert = $this->db->insert('sekolah', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+	
+	function index_put() {
+        $id = $this->put('id');
+        $data = array(
+                    'id'       => $this->put('id'),
+                    'nama'          => $this->put('nama'),
+                    'alamat'    => $this->put('alamat'),
+					'logo' => $this->put('logo'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('sekolah', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+	
+	function index_delete() {
+        $id = $this->delete('id');
+        $this->db->where('id', $id);
+        $delete = $this->db->delete('sekolah');
+        if ($delete) {
+            $this->response(array('status' => 'success'), 201);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+}
